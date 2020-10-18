e0b9db424019ad92384c6271e35e5efa6959f059
ruanramirez <ruanramirez01@gmail.com>
2020-10-18 17:50:48 -0300

    added backend

---
+++ src/database/fakedata.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-9.5623451",
+        lng: "-35.7390768",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 6 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+
+            "https://images.unsplash.com/photo-1595295407820-3563d04518be?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1598454444427-8bffa498b6f2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1597552816350-edd306e5c06f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1597791366640-e91489aaf430?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h até 18h",
+        open_on_weekends: "1"
+
+    },
+
+    {
+        id: 2,
+        lat: "-9.5623451",
+        lng: "-35.7199768",
+        name: "Lar das meninos",
+        description: "Presta assistência a crianças de 6 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+
+            "https://images.unsplash.com/photo-1595295407820-3563d04518be?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1598454444427-8bffa498b6f2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1597552816350-edd306e5c06f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1597791366640-e91489aaf430?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 8h até 18h",
+        open_on_weekends: "0"
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
 //importar dependencia/lib
 const express = require('express'); // variavel que recebe uma funcao
-
+const path = require('path');
+const pages = require('./pages.js');
 //iniciando o express
 const server = express(); // retorno da funcao é um objecto
+server
+    //utilizando os arquivos estáticos
+    .use(express.static('public'))
 
+    //configurar template engines
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'hbs')
 
-//criar uma rota (caminho)
-server.get('/', ()=>{
-    
-})
+    //criar uma rota (caminho)
+    .get('/', pages.index)
+    .get('/orphanage', pages.orphanage)
+    .get('/orphanages', pages.orphanages)
+    .get('/create-orphanage', pages.createOrphanage)
 
 //ligando o servidor
 server.listen(5500)
